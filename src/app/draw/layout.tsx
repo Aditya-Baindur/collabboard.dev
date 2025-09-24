5ade2c78b4095ccfa8d6ff268ea4babd843cf4a5
Aditya Baindur <adityabaindur@icloud.com>
2025-09-24 04:42:27 -0400

    feat: Add gradient background to UI

---
+++ src/app/draw/layout.tsx
@@ -0,0 +1,6 @@
+// Server layout is fine for CSS imports
+import "@excalidraw/excalidraw/index.css";
+
+export default function DrawLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
